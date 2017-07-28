d8be039808e53f1b71ac44cc32a4232934473a83
markdyousef <mark@clai.io>
2017-07-28 16:47:47 +0200

    added notes and code to conv layer and architecture

---
+++ cnn/index.py
@@ -0,0 +1,18 @@
+import numpy as np
+from numpy import random
+
+
+def convolution(input, padding=0, filter_size=(2, 2),
+                stride=2, output_depth=5):
+
+    output_width = int((input.shape[0] - filter_size(0)
+                        + 2*padding) / stride + 1)
+
+    output_height = int((input.shape[1] - filter_size(1)
+                        + 2*padding) / stride + 1)
+
+    filter_ = random.rand(filter_size(0), filter_size(1), output_depth)
+
+    print('output width {}'.format(output_width))
+    print('output_height {}'.format(output_height))
+    print('filter {}'.format(filter_))
